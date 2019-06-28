6970559704a0856f1c8390819a1323ed3651504c
neilujerdan <julien.letouzic1@gmail.com>
2019-06-28 12:59:04 +0200

    position

--- bot/commands/location.rb
+++ bot/commands/location.rb
@@ -1,5 +1,5 @@
 module Commands
-  API_URL = 'https://api.openweathermap.org/data/2.5/weather?lat='.{lat}.'long='.{long}.'§andappid=526286c33895674959f3b36eb9b60f9b'
+  API_URL = 'https://api.openweathermap.org/data/2.5/weather?lat='.lat.'long='.long.'§andappid=526286c33895674959f3b36eb9b60f9b'
 
   # Lookup based on location data from user's device
   def lookup_location
